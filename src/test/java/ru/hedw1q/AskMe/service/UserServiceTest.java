7f55e1ad0c751879eb85aef310be2d04bfaf7d81
Ainur <ryamovainur@gmail.com>
2021-03-06 21:08:15 +0300

    added tests for UserService

--- src/test/java/ru/hedw1q/AskMe/service/UserServiceTest.java
+++ src/test/java/ru/hedw1q/AskMe/service/UserServiceTest.java
@@ -22,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 @SpringBootTest
 public class UserServiceTest {
+
     @Autowired
     private UserService userService;
     @Autowired
